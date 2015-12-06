065984d896e491a8a4c5a0a779e686714c418895
Flacx <blueplant@medionmail.com>
2015-12-06 22:56:27 +0100

    Base Version - Fehlerbehaftet und unfertig

---
+++ Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    private char color;
+    private char number;
+
+    public Card(char c, char n) {
+        color = c;
+        number = n;
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public char getNumber() {
+        return number;
+    }
+}
---
+++ test.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println(s);
+    }
+
+}
