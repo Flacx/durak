aae3c94844d3c65f0af8e9fd35be50e62afa8e31
Confruggy <marcel.p99@gmx.de>
2015-12-07 19:35:10 +0100

    Update

--- test.java
+++ test.java
@@ -6,7 +6,7 @@ public class test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
-        System.out.println(s);
+        System.out.println(s); //test
     }
 
 }
